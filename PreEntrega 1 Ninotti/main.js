97b8e5f5e343ff2c0ab386ca8cdbd772adfeb9d5
mirna ninotti <mir_2401@hotmail.com>
2023-02-28 21:00:55 -0300

    cambios

--- PreEntrega 1 Ninotti/main.js
+++ PreEntrega 1 Ninotti/main.js
@@ -13,28 +13,45 @@ let fechaingresada = prompt("ingrese fecha (dd/mm/aaaa)");
 //     fecha = prompt("ingrese fecha de reserva (dd/mm/aaaa)");
 // }
 
-while (fechaingresada != "10/02/2023");
-{
+while (fechaingresada != "10/02/2023");{
     alert ("Fecha no disponible. Ingrese otra");
-    fecha = prompt ("ingrese fecha (dd/mm/aaaa");
+    fecha = prompt ("ingrese fecha (dd/mm/aaaa)");
 }
 
-alert ("hay disponibilidad en esa fecha")
-// while (fechaingresada != "11/02/2023"); {
-//     alert ("Fecha no disponible. Ingrese otra");
-// }
-// while (fechaingresada != "17/02/2023"); {
-//     alert ("Fecha no disponible. Ingrese otra");
-// }
+alert ("Hay disponibilidad en esa fecha")
 
 
-let habitacion = prompt ("que habitacion estas buscando?");
-let habitacionSimple = 5000  
-let cantidad = prompt ("cuantas habitaciones queres reservar?");
 
-let resultado1 = habitacionSimple * cantidad;
+// let habitacion = prompt ("que habitacion estas buscando?");
+// let habitacionSimple = 5000  
+// let cantidad = prompt ("cuantas habitaciones queres reservar?");
 
-alert ("debes abonar" + "$" + resultado1)
+// let resultado1= habitacionsimple * cantidad
+
+// alert ("debes abonar" + "$" + resultado1)
+
+let habitacion = [
+    {personas: "1", habitacion: "Simple", precio: "9000"},
+    {personas: "2", habitacion: "Doble", precio: "15000"},
+    {personas: "3", habitacion: "Triple", precio: "20000"},
+
+];
+
+let personas = prompt("ingrese la cantidad de personas que desean alojarse");
+let filtrado = habitacion.filter( (item) => item.personas === personas );
 
 
+if (personas<3){
+filtrado.forEach( (item) => {
+    let mensaje = `
+    Personas: ${item.personas}
+    habitacion: ${item.habitacion}
+    precio: ${item.precio}
+    `;
+    alert(mensaje);
+})
+} else {
+    alert("no contamos con habitaciones cuadruples, puede reservar dos dobles o una triple y una individual");
+
+}
 
